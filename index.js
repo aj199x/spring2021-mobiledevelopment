296fb19f67b3821793ca8d7d9bbb87c2b59d956d
aj199x <73663022+aj199x@users.noreply.github.com>
2021-02-03 21:46:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// Import Article
-import Article from './Article.js';
+import Sidebar from './components/Sidebar.js';
 
-// Pass the values to the constructor method of Article
-const a = new Article (
-  {
-    title: "This is the title!",
-    author: "Me",
-    text: "This is an example article!"
-  }
-);
+const s = new Sidebar({
+  menu: [
+    'Albert Eggstein',
+    'Attila the Hen',
+    'Attila the Hen',
+    'Dixie Chick',
+    'Gregory Peck',
+    'Mary Poopins'
+  ]
+});
 
-// Shows its HTML on the console
-console.log(a.render() );
-
-class ArticleTitle {
-
-  constructor(title) {
-    this.props = {};
-    this.props.title = title;
-  }
-
-  render() {
-    return `<h1>${this.props.title}</h1>`;
-  }
-
-}
+/* 
+  TODO: Use the render() method of Sidebar to show HTML response on console. Results should be wrapped in <ul></ul>
+  
+  The final result should look like the following:
+  <ul><li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
+*/
+/*Takes items in const s and merges them into the render function in sidebar.js */
+console.log(s.render() );
