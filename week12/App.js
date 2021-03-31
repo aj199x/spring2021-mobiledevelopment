e5d2523f94aca0433d7e03a5687e1d9cd54b235d
aj199x <73663022+aj199x@users.noreply.github.com>
2021-03-31 14:16:17 -0400

    Week12

---
+++ week12/App.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button } from 'react-native';
+import {Card} from 'react-native-elements'; 
+
+export default function App() {
+ {/*Array used for car variable. Also calls on useState function */}
+  const [car, setCar] = useState();
+ {/*Array used for votes variable. Also calls on useState function and defaults it to false.*/}
+  const [votes, setVotes] = useState(false);
+{/*Function for Supra votes. Changes useState to true.*/}
+  function increaseSupraVote(value){
+    setCar('Supra');
+    setVotes(true);
+  }
+{/*Function for GT-R votes. Changes useState to true.*/}
+  function increaseGTRVote(value){
+    setCar('GT-R');
+    setVotes(true);
+  }
+{/*Function for Demon votes. Changes useState to true.*/}
+  function increaseDemonVote(value){
+    setCar('Demon');
+    setVotes(true);
+  }
+  return (
+    
+      <View style={styles.container}>
+        {/*Displays when user makes a choice*/}
+      { votes ? (
+        <View style={styles.container}>
+          <Text>
+            {/*Uses car variable to tell user they voted for whichever car they chose*/}
+            You voted for {car}!
+          </Text>
+        </View>
+      ) : (
+        /*Displays voting options*/
+        <View>
+        <Text style={styles.headerText}>
+          Vote for your favorite car!
+        </Text>
+      <Card>
+        <Button title="Toyota Supra" onPress={(value) => (increaseSupraVote('Supra'))} />
+      </Card>
+      {/*Divides content*/}
+      <Card.Divider/>
+      <Card>
+        <Button title="Nissan GT-R" onPress={(value) => (increaseGTRVote('GT-R'))} />
+      </Card>
+      <Card.Divider/>
+      <Card>
+        <Button title="Dodge Demon" onPress={(value) => (increaseDemonVote('Demon'))} />
+      </Card>
+      </View>
+      )
+      } 
+    </View>
+  );
+}
+{/*Stylings for the page*/}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold',
+  }
+});
